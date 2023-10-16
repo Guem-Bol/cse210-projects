5e8a2cee7a7cd3ae20b0b8ff8f5f01079d2b7c77
Guem-Bol <132097001+Guem-Bol@users.noreply.github.com>
2023-10-16 12:24:27 +0300

    Add files via upload

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+    List<int> numbers = new List<int>();    
+    int Number = 100;
+    
+    
+
+    Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+    while (Number != 0)
+    
+    {
+        
+        Console.Write("Enter number: ");
+        Number = int.Parse(Console.ReadLine());
+
+         if (Number != 0)
+
+            {
+             numbers.Add(Number);
+            }
+      
+
+      
+        
     }
+
+     int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+
+        int Max = numbers[0];
+
+        foreach (int number in numbers)
+
+        {
+
+            if (number > Max)
+            {
+                Max = number;
+                Console.WriteLine($"The maxinum number is: {Max}");
+               
+            }
+
+        
+        }
+
+
+
+ }
+
+  
 }
