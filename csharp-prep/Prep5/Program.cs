58a8dd17b05e7aac52d899d5cc606a822a4df03c
Guem-Bol <132097001+Guem-Bol@users.noreply.github.com>
2023-10-16 12:26:42 +0300

    Add files via upload

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,65 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+     string inputuserName = DisplayPersonalMessage();
+     int inputNumber = DisplayNumber();
+     All(inputuserName ,inputNumber );
+
+        DisplayMessage();
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        DisplayPersonalMessage();
+
+        static string DisplayPersonalMessage()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+
+        return name;
+            
+            
+        }
+
+        DisplayNumber();
+
+        static int DisplayNumber()
+
+        {
+            int number = 0;
+            Console.Write("Please enter your favorite number: ");
+            number = int.Parse(Console.ReadLine());
+
+            int numb = number * number ;
+
+        return numb;
+
+
+        }
+
+        
+
+        static void All( string inputuserName, int  inputNumber)
+
+        {
+           
+         Console.WriteLine($"{inputuserName},The square of your number is {inputNumber}");
+
+            
+        }
+
+
+
+        
     }
 }
